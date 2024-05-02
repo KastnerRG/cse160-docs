b86beae51b722385d4ff964ed51f843239380789
pvijay03 <pvijay@ucsd.edu>
2024-05-01 17:38:02 -0700

    change to default theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,6 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx_material',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
