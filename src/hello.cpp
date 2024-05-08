d3f1bfba2a034209e6ad3939bbbe94943d9fc877
Ashwin Rohit <ashwinrohit@proton.me>
2024-05-07 19:54:46 -0700

    Add CPP source directory

---
+++ src/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+/*! \verbatim embed:rst:leading-asterisk
+ * Hello, World!
+ * \endverbatim
+*/
+class SampleClass {
+public:
+	/*! \verbatim embed:rst:leading-asterisk
+	 * This function prints hello
+	 * \endverbatim
+	 */
+	static void printHello();
+};
